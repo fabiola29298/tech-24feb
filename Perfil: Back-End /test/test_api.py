c2e699651513c2719d2a60e577fdbfbf076c89ba
Nohbek <136768391+nohbek@users.noreply.github.com>
2025-02-18 15:53:57 -0600

    Create test_api.py
    Pruebas automatizadas

---
+++ Perfil: Back-End /test/test_api.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from src.main import app
+
+client = TestClient(app)
+
+def test_add_user():
+    response = client.post("/add_user/?name=Ana")
+    assert response.status_code == 200
+    assert response.json()["message"] == "User Ana added successfully"
