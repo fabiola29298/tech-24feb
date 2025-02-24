7d6ad428b4f33274c554baf696dcfdba8f6525d8
Fabiola Acarapi <fabiola29298@gmail.com>
2025-02-24 18:47:39 -0400

    Update simple_payment.sol

--- backend-blockchain/simple_payment.sol
+++ backend-blockchain/simple_payment.sol
@@ -1,15 +1,25 @@
+// El smart contract esta en zksync Sepolia
+// 0x9dD30B2Bd7360B591e92C13267326ebD2573cc8D
+// link https://sepolia.explorer.zksync.io/address/0x9dD30B2Bd7360B591e92C13267326ebD2573cc8D
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.19;
+ 
 
-contract SimplePayment {
-    address public owner;
-
-    constructor() {
-        owner = msg.sender;
+contract simplePayment {
+    uint256 myFavoriteNumber;
+    // almacenar el valor
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+    // agregar 1 al valor y almacenarlo
+    function add(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber+1;
+    }
+    // mostrar el valor
+    function retrieve() public view returns (uint256) {
+        return myFavoriteNumber;
     }
 
-    function sendEther(address payable _to) public payable {
-        require(msg.value > 0, "Debe enviar ETH");
-        _to.transfer(msg.value);
-    }
+    
 }
